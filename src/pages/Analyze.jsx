ed67d3a62450653cf604a68c2bc682dfaf394fe5
a⚡︎ay <ajayvarma007j@gmail.com>
2026-03-12 01:26:19 -0700

    suiii

--- src/pages/Analyze.jsx
+++ src/pages/Analyze.jsx
@@ -174,7 +174,7 @@ function Analyze() {
       setScanPhase(2);
       
       const res = await axios.post(
-  "https://cervical-cancer-api-port-8000.up.railway.app",
+  "https://cervical-cancer-api-port-8000.up.railway.app/predict",
   formData
 );
       const predicted = res.data.prediction;
